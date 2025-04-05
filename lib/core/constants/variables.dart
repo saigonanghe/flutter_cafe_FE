c288956c204bda69433900bb9b3e143ce669d3d5
saigonanghe <saigonanghe@gmail.com>
2025-04-05 12:45:26 +0700

    Update and rename variables copy.dart to variables.dart

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,7 +1,7 @@
 class Variables {
   static const String appName = 'Resto App';
   static const String apiVersion = 'v1';
-  static const String baseUrl = 'http://(your ip):8000';
+  static const String baseUrl = 'https://flutter.maxui.net';
 
   // static const String baseUrl = 'http://(your website backend)
 
